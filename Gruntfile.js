dc9e4befe2a7b84209ce1e923dbb3a66b62ed586
John Crisostomo <johncrisostomo@protonmail.ch>
2017-03-08 21:02:19 +0800

    adds mocha testing to task runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,30 +3,39 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    mochaTest: {
+      src: ['test/*.js'],
+      test: {
+        options: {
+          reporter: 'spec',
+        },
+      },
+    },
     browserify: {
       client: {
         src: ['src/index.js'],
         dest: 'dist/cheers-client.js',
         options: {
           browserifyOptions: {
-            standalone: 'cheers'
-          }
-        }
-      }
+            standalone: 'cheers',
+          },
+        },
+      },
     },
     uglify: {
       options: {
-        banner: '/* <%= pkg.name %> <%= pkg.version %> */'
+        banner: '/* <%= pkg.name %> <%= pkg.version %> */',
       },
       build: {
         src: 'dist/cheers-client.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
+        dest: 'dist/<%= pkg.name %>.min.js',
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['browserify', 'uglify']);
+  grunt.registerTask('default', ['mochaTest', 'browserify', 'uglify']);
 }
