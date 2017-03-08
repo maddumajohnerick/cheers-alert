2e767444da90cbe557e0494e5b0856990948de5d
John Crisostomo <johncrisostomo@protonmail.ch>
2017-03-08 21:24:05 +0800

    minifies css file for dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
     browserify: {
       client: {
         src: ['src/index.js'],
-        dest: 'dist/cheers-client.js',
+        dest: 'dist/cheers-alert.js',
         options: {
           browserifyOptions: {
             standalone: 'cheers',
@@ -27,15 +27,27 @@ module.exports = function(grunt) {
         banner: '/* <%= pkg.name %> <%= pkg.version %> */',
       },
       build: {
-        src: 'dist/cheers-client.js',
+        src: 'dist/cheers-alert.js',
         dest: 'dist/<%= pkg.name %>.min.js',
-      }
-    }
+      },
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist',
+          ext: '.min.css',
+        }],
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['mochaTest', 'browserify', 'uglify']);
+  grunt.registerTask('default', ['mochaTest', 'browserify', 'uglify', 'cssmin']);
 }
